15588f7c038d4e9edae56f23f27d8b8ce8b950bb
김도현 <rlaqkqgus@naver.com>
2023-01-20 13:05:05 +0900

    수당

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-!9a*l0ii%iwe0&v%!m&4o^-r!nnw6no(sqlr+$0j@(w!!*wa=q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.35.113', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['13.36.71.247', ' ec2-13-36-71-247.eu-west-3.compute.amazonaws.com']
 
 
 # Application definition
